6d2fb8f355aa73be290f6161fd278a50c0965f9c
SnowWoofer <berealordietrying1@gmail.com>
2026-09-22 04:19:24 +0200

    Minor Syntax: Updated misattributed ca port

--- blockchain/scripts/register-roles.sh
+++ blockchain/scripts/register-roles.sh
@@ -28,7 +28,7 @@ ca_port() {
   case "$1" in
     1) echo 7054 ;;
     2) echo 8054 ;;
-    3) echo 9084 ;;
+    3) echo 9054 ;;
     *) if [ "$1" -ge 4 ]; then echo $((12054 + 100 * ($1 - 4))); else echo ""; fi ;;
   esac
 }
